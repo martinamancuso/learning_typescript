a28811ee125c89199b5b9babf08dd53064d430f5
martinamancuso <martinamancuso24@gmail.com>
2024-03-18 17:02:52 +0100

    first exercise done

---
+++ capitolo_2/primitive_types.ts
@@ -0,0 +1,35 @@
+/*
+START: Follow the instructions below.
+*/
+
+// Add type annotations to each of these variables.
+
+// string
+let country = "Italy";
+
+// undefined
+let year = undefined;
+
+// number
+let averageTemperature = 23.6;
+
+// boolean
+let visited = true;
+
+// null
+let currency = null;
+
+// Change the type annotations on these variables so they are correct.
+
+// previous: boolean
+let population: number = 47_450_795;
+
+// previous: string
+let isSummer: boolean = false;
+
+// previous: number
+let languages: string = "Spanish, English";
+
+// ----
+
+export {};
