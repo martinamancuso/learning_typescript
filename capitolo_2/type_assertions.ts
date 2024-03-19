b990cf9817acdd49502767048284d2bb235dfcbd
martinamancuso <martinamancuso24@gmail.com>
2024-03-19 08:14:47 +0100

    type assertions exercise completed

---
+++ capitolo_2/type_assertions.ts
@@ -0,0 +1,25 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// previous: 'any'
+function getPopulation(): number {
+    return 69_950_850;
+}
+
+// Add a type assertion after the call to `getPopulation()`.
+// Hint: What's the actual type of the value returned by `getPopulation()`?
+
+const population = getPopulation();
+
+// Now you've added a type assertion, there's a type error in the code below.
+// Remove the code that is incorrect.
+
+// previous:
+// console.log(population.toUpperCase());
+
+console.log(population * 2);
+
+// ----
+
+export {};
