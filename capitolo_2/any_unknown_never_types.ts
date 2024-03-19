8196f7cb91cb5c552e98b2235f226f6bd245beba
martinamancuso <martinamancuso24@gmail.com>
2024-03-19 08:24:16 +0100

    any, unknown, never types exercise completed

---
+++ capitolo_2/any_unknown_never_types.ts
@@ -0,0 +1,36 @@
+/*
+START: Follow the instructions below.
+*/
+
+// Add the `any` type to fix the type errors in the following code.
+
+let currency: any = { name: "Indian rupee" };
+
+currency.code = "THB";
+
+currency = "Baht";
+
+// Fix the `if` statement in this function so the type of `value` is narrowed to `string`.
+// previous: `if (typeof value === "number") (...)`
+
+function countryNameLength(value: unknown) {
+    if (typeof value === "string") {
+        console.log(value.length);
+    }
+}
+
+countryNameLength("United States of America");
+
+// Fix the type error we see when calling this function.
+// Hint: Replace one of the `never` types with a different type.
+// previous: `function throwCountryError(message: never): never (...)`
+
+function throwCountryError(message: string): never {
+    throw new Error(`Could not find country: ${message}`);
+}
+
+throwCountryError("Narnia");
+
+// ----
+
+export {};
